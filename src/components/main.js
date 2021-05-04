84ff29ad4e006511107d12877e039a7e6c27db88
Juan José Peña D <jj.penad@uniandes.edu.co>
2021-05-04 13:39:08 -0500

    show userInfo

--- src/components/main.js
+++ src/components/main.js
@@ -3,6 +3,7 @@ import { useAuth0 } from "@auth0/auth0-react";
 import Panel from "./panel";
 import Login from "./login";
 import Logout from "./logout";
+import UserInfo from "./userInfo";
 
 function Main() {
   const { isAuthenticated } = useAuth0();
@@ -13,6 +14,7 @@ function Main() {
       <div>
         <Logout></Logout>
         <h1>Componente principal</h1>
+        <UserInfo></UserInfo>
         <Panel></Panel>
       </div>
     );
